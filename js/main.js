a193f9599e1f7be99414fb848c6a883e42ce6e01
Juan M Frias <jmfrias@episd.org>
2019-02-13 09:51:22 -0700

    Final project files

--- js/main.js
+++ js/main.js
@@ -1,22 +1,84 @@
-function initMap() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var userLocation = {
-      lat: 40.8054491,
-      lng: -73.9654415
-    };
+var vehicleOptions = [
+	{choice: 'cadenza', price: 35000}, 
+	{choice: 'forte', price: 20000}, 
+	{choice: 'optima', price: 29050}, 
+	{choice: 'sedona', price: 38650}, 
+	{choice: 'soul', price: 42200}
+];
+
+var colorOptions = [
+	{choice: 'black', price: 50}, 
+	{choice: 'white', price: 100}, 
+	{choice: 'silver', price: 250}
+	];
+
+var packageOptions = [
+	{choice: 'Rear Camera', price: 150}, 
+	{choice: 'LED Positioning Light', price: 150}, 
+	{choice: 'Rear Camera and LED Positioning Light', price: 200}
+	];
+
+var carSelection = {
+    vehicle: 	{choice: 'Not Selected', price: 0},
+    color: 		{choice: 'Not Selected', price: 0},
+    package: 	{choice: 'Not Selected', price: 0}
+};
+
+$('li').on('click', function() {
+    $('li').removeClass('active');
+    $(this).addClass('active');
+    $('#options-display').empty();
+    var selectedStep = $(this).data('tab');
+    displayPanel(selectedStep);
+});
+
+
+function displayPanel(step) {
+    var source = $('#' + step + '-options-template').html();
+    var template = Handlebars.compile(source);
     
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: userLocation,
-      zoom: 10,
-      scrollwheel: false
-    });
-    
-    var marker = new google.maps.Marker({
-      position: userLocation,
-      map: map,
-      title: 'User Location'
-    });
-  });
+    switch (step) {
+    case 'vehicle':
+        renderTemplate(vehicleOptions, template, step);
+        break;
+    case 'color':
+        renderTemplate(colorOptions, template, step);
+        break;
+    case 'package':
+        renderTemplate(packageOptions, template, step);
+        break;
+    case 'summary':
+        renderTemplate(carSelection, template, step);
+        break
+    }
 }
 
-initMap();
+function renderTemplate(selection, template, step) {
+    if (step != 'summary') {
+        for (var i = 0; i < selection.length; i++) {
+            var html = template(selection[i]);
+            $('#options-display').append(html)
+        }
+    } else {
+        var html = template(selection);
+        $('#options-display').append(html)
+    }
+}
+
+$('.options-container').on('click', 'div[class*="option"]', function() {
+    var currentSelection = $(this).data('panel');
+    carSelection[currentSelection]['choice'] = $(this).data('option');
+    carSelection[currentSelection]['price'] = $(this).data('price');
+    if (carSelection['color']['choice'] !== 'Not Selected' && carSelection['vehicle']['choice'] !== 'Not Selected') {
+        $('.vehicle-display').attr('src', 'assets/' + carSelection['vehicle']['choice'] + '-' + carSelection['color']['choice'] + '.jpg')
+    } else {
+        if (carSelection['vehicle']['choice'] !== 'Not Selected') {
+            $('.vehicle-display').attr('src', 'assets/' + carSelection['vehicle']['choice'] + '.jpg')
+        }
+    };
+    //updateVehicleCost();
+    var vehicleCost = carSelection['color']['price'] + carSelection['vehicle']['price'] + carSelection['package']['price'];
+    $('.cost-display').text('$' + vehicleCost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+});
+
+displayPanel('vehicle');
